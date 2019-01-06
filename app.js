7faa0baedb6c1dc80d7afdc33b89025c4f453503
akashuba <kashuba-aleksandr@mail.ru>
2019-01-06 20:39:47 +0300

    Add cors manually

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var fs = require("fs");
+var multer = require('multer');
+var upload = multer();
+// var cors = require('cors');
 
 var app = express();
-var jsonParser = bodyParser.json();
-
+// var jsonParser = bodyParser.json();
+// app.use(cors());
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/api/wines", function (req, res) {
+  res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
   fs.readFile("./public/winecardsJSON.json", "utf8", function (err, data) {
     if (err) throw err;
     res.send(data);
   });
 });
 
-app.listen(3000);
+app.post("/api/wines/upload", upload.array(), function (req, res) {
+  res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
+  console.log(req.body)
+  res.send('Get your form data!')
+});
+
+app.listen(3004);
